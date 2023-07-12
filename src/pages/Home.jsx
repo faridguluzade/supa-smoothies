9fe9070672ca7acba07d2b39e12fa02095f6eb62
faridguluzade <tu5mhbxzt.code.edu.az>
2023-07-12 00:30:25 -0800

    delete and sorting future was adding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import SmoothieCard from "../components/SmoothieCard";
 const Home = () => {
   const [fetchError, setFetchError] = useState(null);
   const [smoothies, setSmoothies] = useState(null);
+  const [orderBy, setOrderBy] = useState("created_at");
 
   const handleDelete = (id) => {
     setSmoothies((prevSmoothies) => {
@@ -15,7 +16,10 @@ const Home = () => {
 
   useEffect(() => {
     const fetchSmoothies = async () => {
-      const { data, error } = await supabase.from("smoothies").select("*");
+      const { data, error } = await supabase
+        .from("smoothies")
+        .select()
+        .order(orderBy, { ascending: false });
 
       if (error) {
         setFetchError("Could not fetch the smoothies");
@@ -29,14 +33,21 @@ const Home = () => {
     };
 
     fetchSmoothies();
-  }, []);
+  }, [orderBy]);
 
   return (
     <div className="page home">
       {fetchError && <p>{fetchError}</p>}
       {smoothies && (
         <div className="smoothies">
-          {/* order by buttons */}
+          <div className="order-by">
+            <p>Order by:</p>
+            <button onClick={() => setOrderBy("created_at")}>
+              Time Created
+            </button>
+            <button onClick={() => setOrderBy("title")}>Title</button>
+            <button onClick={() => setOrderBy("rating")}>Rating</button>
+          </div>
           <div className="smoothie-grid">
             {smoothies.map((smoothie) => (
               <SmoothieCard
